d5e59c32a0fb75848fd17e5ad339a8558319c965
Alex Linden <alextl2004@gmail.com>
2023-02-06 12:37:30 -0800

    basics of login/profile

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Profile from './components/Profile';
+import Login from './components/Login';
 
 function App() {
   return (
-    <div className="App"> Hello World!
+    <div className="App">
+      <Profile />
+      <Login />
     </div>
   );
 }
